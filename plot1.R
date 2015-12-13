2766cf1e2819b012116ca354603f74cc43e9f552
nadavkri <nadav.krispin@gmail.com>
2015-12-13 23:13:51 +0200

    wow

---
+++ plot1.R
@@ -0,0 +1,5 @@
+library("sqldf")
+data <- read.csv.sql("household_power_consumption.txt",sql = "select * from file where Date in ('1/2/2007','2/2/2007')",sep=";",header=TRUE)
+png('plot1.png')
+hist(data$Global_active_power,col="red",xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,5 @@
+library("sqldf")
+data <- read.csv.sql("household_power_consumption.txt",sql = "select * from file where Date in ('1/2/2007','2/2/2007')",sep=";",header=TRUE)
+png('plot2.png')
+plot(strptime(paste(data$Date,data$Time),format = "%d/%m/%Y %H:%M:%S"),data$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+dev.off()
